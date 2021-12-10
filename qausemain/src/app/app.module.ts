4141ebc986aa25e917ec08cf392806159b60b9bd
Ravi <59188279+Xavi310899@users.noreply.github.com>
2021-12-10 18:49:18 +0530

    second change

--- qausemain/src/app/app.module.ts
+++ qausemain/src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './signup/signup.component';
+import { PhoneComponent } from './phone/phone.component';
+import { OtpComponent } from './otp/otp.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     WelcomeComponent,
+     SignupComponent,
+     PhoneComponent,
+     OtpComponent
   ],
   imports: [
     BrowserModule,
